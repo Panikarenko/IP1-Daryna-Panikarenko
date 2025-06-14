9e5a1dc178ac6675e9932e2a8d97ce3a9f7cdf86
Jakub <jakomacho@gmail.com>
2025-06-14 23:56:20 +0200

    Refactor WatershedVincentSoille: Optimize label assignment and queue management in watershed algorithm, improve color generation for visualization.

--- 01/do_sprawdzenia/segmentation.py
+++ 01/do_sprawdzenia/segmentation.py
@@ -1,6 +1,8 @@
 import numpy as np
 from PIL import Image
 import heapq
+from collections import deque
+from random import randint
 
 class WatershedVincentSoille:
     INIT = -2
@@ -28,8 +30,8 @@ class WatershedVincentSoille:
         neighbors = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
                      (0, 1), (1, -1), (1, 0), (1, 1)]
 
-        for level in range(256):
-            for y, x in sorted_pixels[level]:
+        for h in range(256): 
+            for y, x in sorted_pixels[h]:
                 labels[y, x] = self.MASK
                 for dy, dx in neighbors:
                     ny, nx = y + dy, x + dx
@@ -40,53 +42,47 @@ class WatershedVincentSoille:
                             labels[y, x] = self.INQUEUE
                             break
 
-            cur_dist = 1
             while fifo:
                 d, y, x = heapq.heappop(fifo)
-                if d > cur_dist:
-                    cur_dist = d
+                
                 for dy, dx in neighbors:
                     ny, nx = y + dy, x + dx
                     if 0 <= ny < self.height and 0 <= nx < self.width:
-                        if labels[ny, nx] > 0:
-                            if labels[y, x] == self.INQUEUE or labels[y, x] == self.WSHED:
-                                labels[y, x] = labels[ny, nx]
-                            elif labels[y, x] != labels[ny, nx]:
+                        if labels[ny, nx] >= 0:
+                            if labels[y, x] > 0 and labels[y, x] != labels[ny, nx]:
                                 labels[y, x] = self.WSHED
+                            elif labels[y, x] == self.INQUEUE:
+                                labels[y, x] = labels[ny, nx]
                         elif labels[ny, nx] == self.MASK:
                             dist[ny, nx] = d + 1
-                            heapq.heappush(fifo, (d + 1, ny, nx))
                             labels[ny, nx] = self.INQUEUE
+                            heapq.heappush(fifo, (d + 1, ny, nx))
 
-            for y, x in sorted_pixels[level]:
+            for y, x in sorted_pixels[h]:
                 if labels[y, x] == self.MASK:
                     current_label += 1
+                    queue = deque([(y, x)])
                     labels[y, x] = current_label
-                    queue = [(y, x)]
                     while queue:
-                        qy, qx = queue.pop()
+                        qy, qx = queue.popleft()
                         for dy, dx in neighbors:
                             ny, nx = qy + dy, qx + dx
                             if 0 <= ny < self.height and 0 <= nx < self.width:
                                 if labels[ny, nx] == self.MASK:
                                     labels[ny, nx] = current_label
                                     queue.append((ny, nx))
-
+        
         return self._visualize(labels)
 
     def _visualize(self, labels):
-        from random import randint
-        label_to_color = {
-            self.WSHED: (0, 0, 0)
-        }
+        label_to_color = { self.WSHED: (0, 0, 0) }
         max_label = labels.max()
         for label in range(1, max_label + 1):
-            label_to_color[label] = (randint(50, 255), randint(50, 255), randint(50, 255))
+            label_to_color[label] = (randint(30, 255), randint(30, 255), randint(30, 255))
 
         rgb = np.zeros((self.height, self.width, 3), dtype=np.uint8)
         for y in range(self.height):
             for x in range(self.width):
                 label = labels[y, x]
-                rgb[y, x] = label_to_color.get(label, (0, 0, 0))
-
+                rgb[y, x] = label_to_color.get(label, (255, 255, 255))
         return Image.fromarray(rgb)
